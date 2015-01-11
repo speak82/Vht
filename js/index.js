4592d37ea622826cc1cc7162c9a33e47b39faf0e
speak82 <speak@speak.hu>
2015-01-11 19:30:54 +0100

    first commit

--- js/index.js
+++ js/index.js
@@ -34,6 +34,30 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
 		
+	alert("12");
+		
+	var db = window.sqlitePlugin.openDatabase("Database", "1.0", "Demo", -1);
+alert("12");
+      db.transaction(function(tx) {
+        tx.executeSql('DROP TABLE IF EXISTS test_table');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
+
+        tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
+          alert("insertId: " + res.insertId + " -- probably 1");
+          alert("rowsAffected: " + res.rowsAffected + " -- should be 1");
+
+          tx.executeSql("select count(id) as cnt from test_table;", [], function(tx, res) {
+            alert("res.rows.length: " + res.rows.length + " -- should be 1");
+            alert("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+          });
+
+        }, function(e) {
+          alert("ERROR: " + e.message);
+        });
+      });
+	  
+	  
+		
 		 pictureSource=navigator.camera.PictureSourceType;
          destinationType=navigator.camera.DestinationType;
 		
