73213da856f57f096f96bfffc473bf191c0592a0
speak82 <speak@speak.hu>
2015-01-11 20:43:56 +0100

    first commit

--- js/index.js
+++ js/index.js
@@ -42,13 +42,11 @@ var app = {
         tx.executeSql('DROP TABLE IF EXISTS test_table');
         tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
 
-        tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
-          alert("insertId: " + res.insertId + " -- probably 1");
-          alert("rowsAffected: " + res.rowsAffected + " -- should be 1");
+        tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (1,'nane')")
 
           tx.executeSql("select count(id) as cnt from test_table;", [], function(tx, res) {
             alert("res.rows.length: " + res.rows.length + " -- should be 1");
-            alert("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+            alert("res.rows.item(0).cnt: " + res.rows.item(0).value() + " -- should be 1");
           });
 
         }, function(e) {
