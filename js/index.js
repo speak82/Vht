fb02b5df373dbab0339de5eaeea7f176b56b4448
speak82 <speak@speak.hu>
2015-01-11 19:38:09 +0100

    first commit

--- js/index.js
+++ js/index.js
@@ -34,10 +34,10 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
 		
-	alert("12");
+	
 		
 	var db = window.sqlitePlugin.openDatabase("Database", "1.0", "Demo", -1);
-alert("12");
+
       db.transaction(function(tx) {
         tx.executeSql('DROP TABLE IF EXISTS test_table');
         tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
